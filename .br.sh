bae215da5e3c7d576ba594b5963b7cf48965fbaa
Alexander <alxndr+github@gmail.com>
2013-10-02 11:55:41 -0700

    alias for ack --js

--- .br.sh
+++ .br.sh
@@ -23,4 +23,5 @@ gco() {
 alias update_homepage="if [[ -e ./vendor/plugins/cmservice_plugin/bin/cmservice ]]; then ruby vendor/plugins/cmservice_plugin/bin/cmservice && echo Reloaded homepage; fi"
 alias rr="if [[ -e ./tmp/restart.txt ]]; then touch ./tmp/restart.txt && echo Restarted server; else echo No ./tmp/restart.txt?; fi; update_homepage"
 alias cukes="ENV=local cucumber features/ -t @complete -t @write -r features/"
+alias jack="ack --js --ignore-dir=public/pkg/ --ignore-dir=public/javascripts/vendor/"
 

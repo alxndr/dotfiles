bd8b5d3f04b815665347789c6ecaabc707bd60a7
Alexander <alxndr+github@gmail.com>
2014-01-28 09:36:14 -0800

    br: no smart_asset alias

--- .br.sh
+++ .br.sh
@@ -25,6 +25,5 @@ alias cukes="ENV=local cucumber features/ -t @complete -t @write -r features/"
 alias generate_tags="ctags -R -f ./.git/tags --exclude=jscoverage --exclude=.jhw-cache --exclude=.sass-cache --exclude=pkg ."
 alias jack="ack --js --ignore-dir=pkg --ignore-dir=vendor --ignore-dir=jscoverage --ignore-dir=ext"
 alias rr="if [[ -e ./tmp/restart.txt ]]; then touch ./tmp/restart.txt && echo Restarted server; else echo No ./tmp/restart.txt?; fi; update_homepage"
-alias smart_asset="smart_asset -v"
 alias update_homepage="if [[ -e ./vendor/plugins/cmservice_plugin/bin/cmservice ]]; then ruby vendor/plugins/cmservice_plugin/bin/cmservice && echo Reloaded homepage; fi"
 

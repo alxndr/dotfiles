49bc1f3d657b0b12e9fa0710e8e55ead91bec497
Alexander <alxndr+github@gmail.com>
2014-03-31 09:18:25 -0700

    br: fix assets shortcut

--- .br.sh
+++ .br.sh
@@ -22,8 +22,8 @@ gco() {
 
 alias brr="bundle && rdm && rr"
 alias cukes="ENV=local cucumber features/ -t @complete -t @write -r features/"
+alias fsa="bin/deploy/fix_pkg && echo now running smart_asset... && DEBUG=1 smart_asset"
 alias generate_tags="ctags -R -f ./.git/tags --exclude=jscoverage --exclude=.jhw-cache --exclude=.sass-cache --exclude=pkg ."
-alias fix_and_smart_asset="bin/deploy/fix_pkg && echo now running smart_asset... && DEBUG=1 smart_asset"
 alias jack="ack --js --ignore-dir=pkg --ignore-dir=vendor --ignore-dir=jscoverage --ignore-dir=ext"
 alias rr="if [[ -e ./tmp/restart.txt ]]; then touch ./tmp/restart.txt && echo Restarted server; else echo No ./tmp/restart.txt?; fi; update_homepage"
 alias update_homepage="if [[ -e ./vendor/plugins/cmservice_plugin/bin/cmservice ]]; then ruby vendor/plugins/cmservice_plugin/bin/cmservice && echo Reloaded homepage; fi"

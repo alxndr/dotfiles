b77d0e077c8be62379568b04af7f927ca54dd879
Alexander <alxndr+github@gmail.com>
2014-06-26 16:14:41 -0700

    br aliases: remove gco, add ticket

--- .br.sh
+++ .br.sh
@@ -1,30 +1,11 @@
 # shortcuts and whatnot for br
 
-# git-cycle helper function
-# `gco ticket-number` will check out a branch that includes "ticket-number"
-gco() {
-  if [[ -z "$(which gitc)" ]]; then
-    echo "Can't find gitc?"
-    return
-  fi
-  if [[ -z "$1" ]]; then
-    echo "Need a parameter to search for..."
-    return
-  fi
-  BRANCH=$(git branch | grep -m 1 "$1" | sed -e 's/^..//')
-  echo $BRANCH
-  if [[ -z "$BRANCH" ]]; then
-    echo "No branch found with '$1' in the name."
-    return
-  fi
-  gitc checkout "$BRANCH"
-}
-
 alias brr="bundle && rdm && rr"
 alias cukes="ENV=local cucumber features/ -t @complete -t @write -r features/"
 alias fsa="bin/deploy/fix_pkg && echo now running smart_asset... && DEBUG=1 smart_asset"
 alias generate_tags="ctags -R -f ./.git/tags --exclude=jscoverage --exclude=.jhw-cache --exclude=.sass-cache --exclude=pkg ."
 alias jack="ack --js --ignore-dir=pkg --ignore-dir=vendor --ignore-dir=jscoverage --ignore-dir=ext"
 alias rr="if [[ -e ./tmp/restart.txt ]]; then touch ./tmp/restart.txt && echo Restarted server; else echo No ./tmp/restart.txt?; fi; update_homepage"
+ticket() { gitc branch https://bleacherreport.lighthouseapp.com/projects/6296/tickets/$1 }
 alias update_homepage="if [[ -e ./vendor/plugins/cmservice_plugin/bin/cmservice ]]; then ruby vendor/plugins/cmservice_plugin/bin/cmservice && echo Reloaded homepage; fi"
 

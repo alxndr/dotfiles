c416a8c01437c07b07f0a78ab21b569f75834869
Alexander <alxndr+github@gmail.com>
2014-07-09 11:59:46 -0700

    aliases: some git ones, branch, us

--- .br.sh
+++ .br.sh
@@ -6,6 +6,6 @@ alias fsa="bin/deploy/fix_pkg && echo now running smart_asset... && DEBUG=1 smar
 alias generate_tags="ctags -R -f ./.git/tags --exclude=jscoverage --exclude=.jhw-cache --exclude=.sass-cache --exclude=pkg ."
 alias jack="ack --js --ignore-dir=pkg --ignore-dir=vendor --ignore-dir=jscoverage --ignore-dir=ext"
 alias rr="if [[ -e ./tmp/restart.txt ]]; then touch ./tmp/restart.txt && echo Restarted server; else echo No ./tmp/restart.txt?; fi; update_homepage"
-ticket() { gitc branch https://bleacherreport.lighthouseapp.com/projects/6296/tickets/$1 }
+      ticket() { gitc branch https://bleacherreport.lighthouseapp.com/projects/6296/tickets/$1 }
 alias update_homepage="if [[ -e ./vendor/plugins/cmservice_plugin/bin/cmservice ]]; then ruby vendor/plugins/cmservice_plugin/bin/cmservice && echo Reloaded homepage; fi"
 

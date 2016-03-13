bc5f800912bd215dea0f48ebcf7615f78a0bf6ef
Alexander <alxndr+github@gmail.com>
2016-03-13 15:16:00 -0700

    iex: reorder

--- .iex.exs
+++ .iex.exs
@@ -1,10 +1,10 @@
-if Ecto do
-  import Ecto.Query, only: [limit: 2, select: 3, where: 3]
-end
-
 IEx.configure(
   alive_prompt: "%prefix(%node):%counter>",
   colors: [eval_result: [:green]],
   default_prompt: "%prefix:%counter>",
   inspect: [limit: -1],
 )
+
+if Ecto.Query do
+  import Ecto.Query, only: [limit: 2, select: 3, where: 3]
+end

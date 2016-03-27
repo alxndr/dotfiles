35ef90b8e66a11ffa1dbc943100c74dcb32723fd
Alexander <alxndr+github@gmail.com>
2016-03-27 11:40:59 -0700

    whitespace

--- .br.sh
+++ .br.sh
@@ -1,8 +1,8 @@
 # shortcuts and whatnot for br
 
-alias cukes="ENV=local cucumber features/ -t @complete -t @write -r features/"
+alias   cukes="ENV=local cucumber features/ -t @complete -t @write -r features/"
 alias   dsh="docker exec -it breport_web_1 bash"
 unalias gc
-      gc() { git co "feature/$1" }
-alias generate_tags="ctags -R -f ./.git/tags --exclude=jscoverage --exclude=.jhw-cache --exclude=.sass-cache --exclude=pkg --exclude=target ."
-alias jack="ack --js --ignore-dir=pkg --ignore-dir=vendor --ignore-dir=jscoverage --ignore-dir=ext"
+        gc() { git co "feature/$1" }
+alias   generate_tags="ctags -R -f ./.git/tags --exclude=jscoverage --exclude=.jhw-cache --exclude=.sass-cache --exclude=pkg --exclude=target ."
+alias   jack="ack --js --ignore-dir=pkg --ignore-dir=vendor --ignore-dir=jscoverage --ignore-dir=ext"

8fc6502d0ec98d99650293cabc9a3ba56761823b
Alexander <alxndr+github@gmail.com>
2016-05-16 15:23:08 -0700

    iex: detect Ecto presence better

--- .iex.exs
+++ .iex.exs
@@ -5,3 +5,18 @@ IEx.configure(
   inspect: [limit: -1],
 )
 
+defmodule EctoHelpers do
+  defmacro __using__(_) do
+    quote do
+      IO.puts "importing Ecto.Query.{limit, select, where} and Ecto.Query.API.fragment"
+      import Ecto.Query, only: [limit: 2, select: 3, where: 3]
+      import Ecto.Query.API, only: [fragment: 1]
+      :ok
+    end
+  end
+end
+
+case Code.ensure_loaded(Ecto) do
+  {:module, _} -> IO.puts "\nEcto detected... get helpers in scope:\n> use EctoHelpers\n"
+  {:error,  _} -> nil
+end

28c712333e7fc1ac9b7b4dfc72bc1b47fb869711
Alexander <alxndr+github@gmail.com>
2017-10-30 14:21:37 -0700

    alias: some git goodies

--- .br.sh
+++ .br.sh
@@ -6,8 +6,11 @@ unalias gc
         gc() { git co "feature/$1" }
         gcC() { git co "feature/CONS-$1" }
         gcP() { git co "feature/CMS-$1" }
+        gcS() { git co "feature/SOC-$1" }
+        gfsC() { git bflow feature start "CONS-$1" }
 alias   generate_tags="ctags -R -f ./.git/tags --exclude=jscoverage --exclude=.jhw-cache --exclude=.sass-cache --exclude=pkg --exclude=target ."
 alias   jack="ack --js --ignore-dir=pkg --ignore-dir=vendor --ignore-dir=jscoverage --ignore-dir=ext"
+
         ticket() {
           if [[ -n "$1" ]]; then
             IDENTIFIER=$1
@@ -16,4 +19,13 @@ alias   jack="ack --js --ignore-dir=pkg --ignore-dir=vendor --ignore-dir=jscover
             IDENTIFIER=${GITBRANCH##*/}
           fi
           open https://statmilk.atlassian.net/browse/$IDENTIFIER
+        }
+
+        gmd() { # git merge develop
+          if [[ -z "$1" ]]; then
+            echo "Uh oh, specify a branch to merge develop into."
+            echo "Exiting!"
+            return 1
+          fi
+          git checkout develop && git pull && git checkout $1 && git pull && git merge develop && echo "Branch develop successfully merged into $1 ."
         }

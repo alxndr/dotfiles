3b1e3fdfcf7c5bfb730b163612bbdac3c75ca889
Alexander <alxndr+github@gmail.com>
2019-03-13 10:48:50 -0700

    br stuff

--- .br.sh
+++ .br.sh
@@ -2,12 +2,12 @@
 
 alias   cukes="ENV=local cucumber features/ -t @complete -t @write -r features/"
 alias   dsh="docker exec -it breport_web_1 bash"
-unalias gc
-        gc() { git co "feature/$1" }
+unalias gcf
+        gcf() { git co "feature/$1" }
         gcC() { git co "feature/CONS-$1" }
-        gcP() { git co "feature/CMS-$1" }
+        gcN() { git co "feature/NR-$1" }
+        gcP() { git co "feature/PT-$1" }
         gcS() { git co "feature/SOC-$1" }
-        gfsC() { git bflow feature start "CONS-$1" }
 alias   generate_tags="ctags -R -f ./.git/tags --exclude=jscoverage --exclude=.jhw-cache --exclude=.sass-cache --exclude=pkg --exclude=target ."
 alias   jack="ack --js --ignore-dir=pkg --ignore-dir=vendor --ignore-dir=jscoverage --ignore-dir=ext"
 

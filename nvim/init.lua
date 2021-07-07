fe912bb026188dbc00e0f4a131b78013fd2e6f19
Alexander <alxndr+github@gmail.com>
2021-07-07 12:33:03 -0700

    nvim: extract plugins into separate file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,105 +1,12 @@
--- helpful aliases
 local cmd = vim.cmd  -- execute Vim commands
-local fn = vim.fn    -- call Vim functions
 local g = vim.g      -- access global variables
 
 g.mapleader = '\\'
 
--- package manager
-require 'paq-nvim' {
-  'savq/paq-nvim'; -- paq-nvim manages itself
-  'ojroques/nvim-bufdel'; -- buffer deletion made saner
-  'winston0410/commented.nvim'; -- commenting shortcuts
-  'voldikss/vim-floaterm'; -- terminal eyecandy
-  'tpope/vim-fugitive'; -- Git helpers
-  'airblade/vim-gitgutter'; -- for GitGutterUndoHunk
-  'cohama/lexima.vim'; -- matched-pair character closing
-  'ggandor/lightspeed.nvim'; -- cursor navigation shortcuts
-  'neovim/nvim-lspconfig';
-  'hoob3rt/lualine.nvim'; -- status line
-  'airblade/vim-rooter'; -- keep vim working directory set to project root
-  'chrisbra/Recover.vim'; -- add Compare to swapfile actions
-  'camspiers/snap'; -- file / buffer finder
-  'mhinz/vim-startify'; -- startup screen
-  'tpope/vim-surround'; -- matched-pair character shortcuts
-  'jacoborus/tender.vim'; -- color scheme
-  'kyazdani42/nvim-tree.lua'; -- file browser
-  'nvim-treesitter/nvim-treesitter';
-  'kyazdani42/nvim-web-devicons'; -- required by nvim-tree ... but doesn't seem to work
-}
-
+require 'plugins'
 require 'options'
 require 'mappings'
 
 cmd 'syntax enable'
-cmd 'colorscheme tender'
-
+cmd 'colorscheme tender' -- installed via plugins
 cmd 'hi FloatermNC guibg=gray'
-
--- commented config
-require('commented').setup {
-  keybindings = {n = 'gc', v = 'gc', nl = 'gcc'},
-}
-
--- lexima config
-cmd([[
-  call lexima#add_rule({ 'char': '=', 'at': ')\%#', 'input': ' => ', 'filetype': ['javascript', 'javascriptreact', 'jasmine.javascript'] })
-  call lexima#add_rule({ 'char': '{', 'at': ')\%#', 'input': ' => {', 'input_after': '}', 'filetype': ['javascript', 'javascriptreact', 'jasmine.javascript'] })
-  call lexima#add_rule({ 'char': '(', 'at': 'cl\%#', 'input': '<BS><BS>global.console.log(', 'input_after': ')', 'filetype': ['javascript', 'javascriptreact', 'jasmine.javascript'] })
-]])
-
-
--- lualine config
-require'lualine'.setup{
-  options = {
-    icons_enabled = true,
-    component_separators = {'', ''},
-    section_separators = {'', ''},
-    theme = 'seoul256',
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {'filename'},
-    lualine_x = {},
-    lualine_y = {'filetype'},
-    lualine_z = {'progress', 'location'}
-  },
-}
-
-
--- startify config
--- h/t https://github.com/mhinz/vim-startify/wiki/Example-configurations#show-modified-and-untracked-git-files
--- TODO fix these functions...
---[[
-  function! GitFilesModified()
-    let files = systemlist('git ls-files -m 2>/dev/null')
-    return map(files, "{'line': v:val, 'path': v:val}")
-  endfunction
-  function! GitFilesUntracked()
-    let files = systemlist('git ls-files -o --exclude-standard 2>/dev/null')
-    return map(files, "{'line': v:val, 'path': v:val}")
-  endfunction
-]]
-g.startify_custom_header = ''
-g.startify_lists = {
-  { type='files',                   header={' ➤ recent global'}           },
-  { type='dir',                     header={' ➤ recent in '..fn.getcwd()} },
-  { type='sessions',                header={' ➤ Sessions'}                },
-  { type='bookmarks',               header={' ➤ Bookmarks'}               },
---{ type=fn['GitFilesModified'](),  header={' ➤ git modified'}            },
---{ type=fn['GitFilesUntracked'](), header={' ➤ git untracked'}           },
-  { type='commands',                header={' ➤ Commands'}                },
-}
-
--- tree config
-g.nvim_tree_quit_on_open = 1
-g.nvim_tree_add_trailing = 1
-
--- treesitter config
-require('nvim-treesitter.configs').setup {
-  ensure_installed = {'bash', 'comment', 'css', 'dockerfile', 'elixir', 'graphql', 'html', 'javascript', 'json', 'lua', 'ruby', 'scss', 'yaml'},
-}
-
--- web-devicons config
--- require('nvim-web-devicons').setup()
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,163 @@
+local cmd = vim.cmd  -- execute Vim commands
+local fn = vim.fn    -- call Vim functions
+local g = vim.g      -- access global variables
+
+-- Paq: package manager
+-- installation instructions:
+-- https://github.com/savq/paq-nvim/blob/cdde12dfbe/README.md#installation
+
+require 'paq-nvim' {
+  'savq/paq-nvim'; -- paq-nvim manages itself
+  'ojroques/nvim-bufdel'; -- buffer deletion made saner
+  'winston0410/commented.nvim'; -- commenting shortcuts
+  'voldikss/vim-floaterm'; -- terminal eyecandy
+  'tpope/vim-fugitive'; -- Git helpers
+  'airblade/vim-gitgutter'; -- for GitGutterUndoHunk
+  'cohama/lexima.vim'; -- matched-pair character closing
+  'ggandor/lightspeed.nvim'; -- cursor navigation shortcuts
+  'neovim/nvim-lspconfig';
+  'hoob3rt/lualine.nvim'; -- status line
+  'airblade/vim-rooter'; -- keep vim working directory set to project root
+  'chrisbra/Recover.vim'; -- add Compare to swapfile actions
+  'camspiers/snap'; -- file / buffer finder
+  'mhinz/vim-startify'; -- startup screen
+  'tpope/vim-surround'; -- matched-pair character shortcuts
+  'jacoborus/tender.vim'; -- color scheme
+  'kyazdani42/nvim-tree.lua'; -- file browser
+  'nvim-treesitter/nvim-treesitter';
+  'kyazdani42/nvim-web-devicons'; -- required by nvim-tree ... but doesn't seem to work
+}
+
+-- commented config
+require('commented').setup {
+  keybindings = {n = 'gc', v = 'gc', nl = 'gcc'},
+}
+
+-- lexima config
+cmd([[
+  call lexima#add_rule({ 'char': '=', 'at': ')\%#', 'input': ' => ', 'filetype': ['javascript', 'javascriptreact', 'jasmine.javascript'] })
+  call lexima#add_rule({ 'char': '{', 'at': ')\%#', 'input': ' => {', 'input_after': '}', 'filetype': ['javascript', 'javascriptreact', 'jasmine.javascript'] })
+  call lexima#add_rule({ 'char': '(', 'at': 'cl\%#', 'input': '<BS><BS>global.console.log(', 'input_after': ')', 'filetype': ['javascript', 'javascriptreact', 'jasmine.javascript'] })
+]])
+
+-- lsp config
+local lspc = require'lspconfig'
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+lspc.cssls.setup{
+  capabilities = capabilities,
+}
+-- TODO: `eslint_d` seems to chew up CPU after running for a few minutes 😞
+-- local function eslint_config_exists()
+  -- local eslintrc = fn.glob('.eslintrc*', 0, 1)
+  -- if not vim.tbl_isempty(eslintrc) then
+    -- return true
+  -- end
+  -- if fn.glob('package.json') and fn.filereadable('package.json') then
+    -- if fn.json_decode(fn.readfile('package.json'))['eslintConfig'] then
+      -- return true
+    -- end
+  -- end
+  -- return false
+-- end
+-- local eslint = {
+  -- lintCommand = 'eslint_d --format visualstudio --stdin --stdin-filename ${INPUT}',
+  -- lintStdin = true,
+  -- lintFormats = {'%f:%l:%c: %m'},
+  -- lintIgnoreExitCode = true,
+  -- formatCommand = 'eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}',
+  -- formatStdin = true
+-- }
+-- lspc.efm.setup{
+  -- on_attach = function(client)
+    -- client.resolved_capabilities.document_formatting = true
+    -- client.resolved_capabilities.goto_definition = false
+    -- -- set_lsp_config(client)
+  -- end,
+  -- root_dir = function()
+    -- if not eslint_config_exists() then
+      -- return nil
+    -- end
+    -- return fn.getcwd()
+  -- end,
+  -- filetypes = {
+    -- 'javascript',
+    -- 'javascriptreact',
+    -- 'javascript.jsx',
+    -- 'json',
+  -- },
+  -- settings = {
+    -- languages = {
+      -- javascript = {eslint},
+      -- javascriptreact = {eslint},
+      -- ['javascript.jsx'] = {eslint},
+      -- json = {eslint},
+    -- },
+  -- },
+-- }
+lspc.html.setup{
+  capabilities = capabilities,
+}
+
+-- lualine config
+require'lualine'.setup{
+  options = {
+    icons_enabled = true,
+    component_separators = {'', ''},
+    section_separators = {'', ''},
+    theme = 'seoul256',
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {'filename'},
+    lualine_x = {},
+    lualine_y = {'filetype'},
+    lualine_z = {'progress', 'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  },
+}
+
+
+-- startify config
+-- h/t https://github.com/mhinz/vim-startify/wiki/Example-configurations#show-modified-and-untracked-git-files
+-- TODO fix these functions...
+--[[
+  function! GitFilesModified()
+    let files = systemlist('git ls-files -m 2>/dev/null')
+    return map(files, "{'line': v:val, 'path': v:val}")
+  endfunction
+  function! GitFilesUntracked()
+    let files = systemlist('git ls-files -o --exclude-standard 2>/dev/null')
+    return map(files, "{'line': v:val, 'path': v:val}")
+  endfunction
+]]
+g.startify_custom_header = ''
+g.startify_lists = {
+  { type='files',                   header={' ➤ recent global'}           },
+  { type='dir',                     header={' ➤ recent in '..fn.getcwd()} },
+  { type='sessions',                header={' ➤ Sessions'}                },
+  { type='bookmarks',               header={' ➤ Bookmarks'}               },
+--{ type=fn['GitFilesModified'](),  header={' ➤ git modified'}            },
+--{ type=fn['GitFilesUntracked'](), header={' ➤ git untracked'}           },
+  { type='commands',                header={' ➤ Commands'}                },
+}
+
+-- tree config
+g.nvim_tree_quit_on_open = 1
+g.nvim_tree_add_trailing = 1
+
+-- treesitter config
+require('nvim-treesitter.configs').setup {
+  ensure_installed = {'bash', 'comment', 'css', 'dockerfile', 'elixir', 'graphql', 'html', 'javascript', 'json', 'lua', 'ruby', 'scss', 'yaml'},
+}
+
+-- web-devicons config
+require('nvim-web-devicons').setup()

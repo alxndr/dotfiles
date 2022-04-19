dd8812f27484527d14d9d91cf6776a6ee75c8783
Alexander <alxndr+github@gmail.com>
2022-04-19 13:37:27 -0700

    nvim: tweak `lualine` to behave with `nvim-tree`

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -226,6 +226,9 @@ require'lualine'.setup{
     section_separators = '',
     theme = 'catppuccin',
   },
+  extensions = {
+    'nvim-tree'
+  },
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff'},
